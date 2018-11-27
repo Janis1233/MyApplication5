3091f2f6a01fda921555f86477ffaddd3c5f84de
Janis1233 <janismarabou@gmail.com>
2018-11-28 00:01:15 +0100

    Test

---
+++ app/src/main/java/com/example/janis/myapplication/HilfeActivity.java
@@ -0,0 +1,48 @@
+package com.example.janis.myapplication;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class HilfeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hilfe);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.essen:
+                        Intent essenIntent =new Intent(getApplicationContext(), EssenActivity.class);
+                        startActivity(essenIntent);
+                        break;
+
+                    case R.id.bezahlungen:
+                        Intent bezahlungIntent =new Intent(getApplicationContext(), BezahlungActivity.class);
+                        startActivity(bezahlungIntent);
+                        break;
+
+                    case R.id.einstellungen:
+                        Intent einstellungenIntent = new Intent(getApplicationContext(), EinstellungenActivity.class);
+                        startActivity(einstellungenIntent);
+                        break;
+
+                    case R.id.profil:
+                        Intent profilIntent =new Intent(getApplicationContext(), ProfilActivity.class);
+                        startActivity(profilIntent);
+                        break;
+                }
+                return true;
+
+            }
+        });
+    }
+}
